8c2056f15d870d9807a21e6626366439c510bf51
MatteoCantafio25 <mattyteo004@gmail.com>
2024-01-16 18:10:44 +0100

    added second exercise script

---
+++ script.js
@@ -0,0 +1,40 @@
+//---------------------------------//
+// MI ASSICURO CHE IL FOGLIO JS COMUNICHI CON IL FOGLIO HTML
+
+console.log("JS OK");
+
+// SCRIVO L'ARRAY DI OGGETTI
+const students = [
+    { id: 213, name: "Marco della Rovere", grades: 78 },
+    { id: 110, name: "Paola Cortellessa", grades: 96 },
+    { id: 250, name: "Andrea Mantegna", grades: 48 },
+    { id: 145, name: "Gaia Borromini", grades: 74 },
+    { id: 196, name: "Luigi Grimaldello", grades: 68 },
+    { id: 102, name: "Piero della Francesca", grades: 50 },
+    { id: 120, name: "Francesca da Polenta", grades: 84 }
+];
+
+// DALL'ARRAY DI PARTENZA CREO UNA LISTA CHE HA COME CONTENUTO SOLO STUDENTI CON VOTO MAGGIORE A 70
+const gradesOver70 = students.filter(({ grades }) => grades > 70)
+
+// STAMPO IN CONSOLE
+console.log(gradesOver70);
+
+// DALL'ARRAY DI PARTENZA CREO UNA LISTA CHE HA COME CONTENUTO SOLO STUDENTI CON VOTO MAGGIORE A 70 E ID MAGGIORE A 120
+const gradesOver70IdOver120 = gradesOver70.filter(({ id }) => id > 120)
+
+// STAMPO IN CONSOLE
+console.log(gradesOver70IdOver120);
+
+
+// DALL'ARRAY DI PARTENZA CREO UNA LISTA CHE HA COME CONTENUTO I NOMI DEGLI STUDENTI MA SCRITTI IN MAIUSCOLO
+const upperNames = students.map(({ name }) => {
+    return name.toUpperCase();
+});
+
+// STAMPO IN CONSOLE
+console.log(upperNames);
+
+
+
+
