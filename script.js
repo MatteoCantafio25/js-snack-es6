e68e4d651cfbdb30fc1e05fdfd2fe3432e7fafb8
MatteoCantafio25 <mattyteo004@gmail.com>
2024-01-17 22:10:09 +0100

    exercise 3-4 script completed

--- script.js
+++ script.js
@@ -45,13 +45,41 @@ console.log(guestsList);
 
 // SCRIVO L'ARRAY DI OGGETTI
 const students = [
-    { id: 213, name: "Marco della Rovere", grades: 78 },
-    { id: 110, name: "Paola Cortellessa", grades: 96 },
-    { id: 250, name: "Andrea Mantegna", grades: 48 },
-    { id: 145, name: "Gaia Borromini", grades: 74 },
-    { id: 196, name: "Luigi Grimaldello", grades: 68 },
-    { id: 102, name: "Piero della Francesca", grades: 50 },
-    { id: 120, name: "Francesca da Polenta", grades: 84 }
+    {
+        id: 213,
+        name: "Marco della Rovere",
+        grades: 78,
+    },
+    {
+        id: 110,
+        name: "Paola Cortellessa",
+        grades: 96,
+    },
+    {
+        id: 250,
+        name: "Andrea Mantegna",
+        grades: 48,
+    },
+    {
+        id: 145,
+        name: "Gaia Borromini",
+        grades: 74,
+    },
+    {
+        id: 196,
+        name: "Luigi Grimaldello",
+        grades: 68,
+    },
+    {
+        id: 102,
+        name: "Piero della Francesca",
+        grades: 50,
+    },
+    {
+        id: 120,
+        name: "Francesca da Polenta",
+        grades: 84,
+    }
 ];
 
 // DALL'ARRAY DI PARTENZA CREO UNA LISTA CHE HA COME CONTENUTO SOLO STUDENTI CON VOTO MAGGIORE A 70
@@ -74,5 +102,136 @@ const upperNames = students.map(({ name }) => {
 
 // STAMPO IN CONSOLE
 console.log(upperNames);
+
+
+//! TERZO ESERCIZIO
+
+
+// SCRIVO L'ARRAY DI OGGETTI
+const bikes = [
+    {
+        name: "Fury",
+        weight: 6.9,
+    },
+    {
+        name: "Fireborn",
+        weight: 9.6,
+    },
+    {
+        name: "Cruz",
+        weight: 4.8,
+    },
+    {
+        name: "Nap",
+        weight: 7.4,
+    },
+    {
+        name: "JJ",
+        weight: 6.8,
+    },
+    {
+        name: "Cloud",
+        weight: 5.0,
+    },
+    {
+        name: "Flash",
+        weight: 8.4,
+    }
+];
+
+// CREO UN FLAG SUPPONENDO CHE LA PRIMA BICICLETTA SIA LA PIU' LEGGERA
+let lightestBike = bikes[0];
+
+// CICLO TUTTE LE BICICLETTE PER CONTROLLARLE IL PESO UNA AD UNA
+for (let i = 0; i < bikes.length; i++) {
+
+    // CREO UNA COSTANTE BIKE
+    const bike = bikes[i];
+    // RICAVO LA BICICLETTA PIU' LEGGERA
+    if (bike.weight < lightestBike.weight) {
+        // CAMBIO IL VALORE INIZIALE CON LA BICICLETTA EFFETTIVAMENTE TROVATA
+        lightestBike = bike;
+    }
+}
+
+// STAMPO IN CONSOLE LA BICICLETTA PIU' LEGGERA
+console.log(lightestBike)
+
+// DESTRUCTURING DELLA BICICLETTA PIU' LEGGERA
+const { name, weight } = lightestBike;
+
+// STAMPO IN CONSOLE UN MESSAGGIO CON GLI ELEMENTI DINAMICI OTTENUTI DAL DESTRUCTURING
+console.log(`La bici più leggera è ${name} con un peso di ${weight} kg.`);
+
+
+
+
+//! QUARTO ESERCIZIO
+
+
+// CREO UNA FUNZIONE PER GENERARE UN NUMERO RANDOM DA MIN A MAX CON MAX INCLUSO
+const getRandomNumber = (min, max) => {
+    const randomNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
+    return randomNumber
+}
+
+
+// SCRIVO L'ARRAY DI OGGETTI 
+const teams = [
+    {
+        name: "Napoli",
+        mp: 0,
+        sf: 0,
+    },
+    {
+        name: "Juventus",
+        mp: 0,
+        sf: 0,
+    },
+    {
+        name: "Milan",
+        mp: 0,
+        sf: 0,
+    },
+    {
+        name: "Vigor Lamezia",
+        mp: 0,
+        sf: 0,
+    },
+    {
+        name: "Cosenza",
+        mp: 0,
+        sf: 0,
+    },
+];
+
+// CREO 2 COSTANTI CHE DETERMINANO IL NUMERO MINIMO E MASSIMO CHE POI ANDRANNO NELLA FUNZIONE PRECEDENTEMENTE CREATA
+
+const minNumber = 1;
+const maxNumber = 30;
+const { sf } = teams
+
+// CICLO L' ARRAY ORIGINALE PER ASSEGNARE UN NUMERO RANDOM A MP = (MADE POINTS) E SF = (SUFFERED FOULS)
+for (let i = 0; i < teams.length; i++) {
+    const team = teams[i];
+    team.mp = getRandomNumber(minNumber, maxNumber);
+    team.sf = getRandomNumber(minNumber, maxNumber);
+}
+
+console.log(sf)
+// STAMPO IN CONSOLE
+console.log(teams);
+
+// CREO UN NUOVO ARRAY SENZA TOCCARE L'ORIGINALE PER AVERE SOLO PARAMETRI DECISI DA ME
+const teamFouls = teams.map(({ name }) => {
+    // CREO IL NUOVO OGGETTO CHE ANDRA' NELL'ARRAY
+    return {
+        Nome: name,
+        sf: getRandomNumber(minNumber, maxNumber),
+    };
+});
+
+// STAMPO IN CONSOLE
+console.log(teamFouls);
 
 
